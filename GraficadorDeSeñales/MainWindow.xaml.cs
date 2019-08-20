163509831bf37d6d14742185364a2b190e6fdf2a
sofiabelmar <sofiabelmar@users.noreply.github.com>
2019-08-20 11:45:25 -0700

    holi 3

--- GraficadorDeSeñales/MainWindow.xaml.cs
+++ GraficadorDeSeñales/MainWindow.xaml.cs
@@ -42,8 +42,14 @@ namespace GraficadorDeSeñales
             plnGrafica.Points.Clear();
             for (double i = tiempoInicial; i <= tiempoFinal; i += periodoMuestreo)
             {
-                plnGrafica.Points.Add(new Point(i * srcGrafica.Width, -1 * (señal.evaluar(i) * srcGrafica.Height/2.0)));  
+                plnGrafica.Points.Add(adaptarCoordenadas(i, señal.evaluar(i)));
             }
+        }
+
+        public Point adaptarCoordenadas(double x, double y)
+        {
+
+            return new Point(x*srcGrafica.Width, (-1 * ( y * ((srcGrafica.Height/2.0) -25))) + (srcGrafica.Height/2));
         }
     }
 }
